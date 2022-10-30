5d5425c38e38de9eb63a7c3306f53b29c6fd8b22
yassineDever <ybouanane.yassine@gmail.com>
2022-10-30 21:39:42 +0000

    first commit with home page

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("main process working");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require('url');
+
+let win;
+
+function createwindow(){
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    win.on('closed',()=>{
+        win = null;
+    })
+}
+app.on('ready',createwindow);
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+app.on('activate',()=>{
+    if(win===null){
+        createwindow();
+    }
+})
